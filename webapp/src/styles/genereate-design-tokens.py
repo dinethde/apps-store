06661c7b08956743481ca610daf3013838061c8f
Dineth De Silva <dinethran33@gmail.com>
2026-06-24 20:34:28 +0530

    feat(tokens): Implement background token resolution

--- webapp/src/styles/genereate-design-tokens.py
+++ webapp/src/styles/genereate-design-tokens.py
@@ -32,5 +32,42 @@ def build_color_palette(source):
     return resolved
 
 
+def build_bg_tokens(bg_variables, pallete):
+    main = {}
+    for categorries, items in bg_variables.items():
+        groups = {}
+        print(categorries)
+        for groups, sub_category in items.items():
+            print(groups)
+            sub_categories = {}
+            for stg, value in sub_category.items():
+                print(stg)
+                values = {}
+                for c, g in value.items():
+                    raw = g["value"]
+
+                    if raw.startswith("{"):
+                        path = raw.strip("{}").split(".")
+                        # print(palette[path[1][path[2]]])
+                        # print(path[1], path[2])
+                        # print(palette[path[1]][path[2]])
+                        values[c] = palette[path[1]][path[2]]
+
+                    else:
+                        values[c] = raw
+
+                print(values)
+
+
+def build_variables(variables, pallete):
+    for v, i in variables.items():
+        if v == "bg":
+            build_bg_tokens(i, pallete)
+
+
 palette = build_color_palette(data)
-print(palette)
+# print(palette)
+
+color_variables = data["color variables"]
+resolved_variables = build_variables(color_variables, palette)
+print(resolved_variables)
