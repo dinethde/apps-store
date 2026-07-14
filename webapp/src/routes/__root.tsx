ab308bf5a7381a5cd1992dbdbe64037ef344a0fd
Dineth De Silva <dinethran33@gmail.com>
2026-07-14 17:55:11 +0530

    feat: add sidebar navigation to root application layout

--- webapp/src/routes/__root.tsx
+++ webapp/src/routes/__root.tsx
@@ -1,10 +1,13 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { ArrowLeftToLine } from 'lucide-react';
+import Logo from "@assets/logo.svg?react"
+import { SquareLibrary } from 'lucide-react';
+
 
 import appCss from '../styles.css?url'
-
-const THEME_INIT_SCRIPT = `(function(){try{var stored=window.localStorage.getItem('theme');var mode=(stored==='light'||stored==='dark'||stored==='auto')?stored:'auto';var prefersDark=window.matchMedia('(prefers-color-scheme: dark)').matches;var resolved=mode==='auto'?(prefersDark?'dark':'light'):mode;var root=document.documentElement;root.classList.remove('light','dark');root.classList.add(resolved);if(mode==='auto'){root.removeAttribute('data-theme')}else{root.setAttribute('data-theme',mode)}root.style.colorScheme=resolved;}catch(e){}})();`
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from '@/components/ui/sidebar';
 
 export const Route = createRootRoute({
   head: () => ({
@@ -35,7 +38,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
         <HeadContent />
       </head>
       <body>
@@ -59,8 +61,64 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 
 function RootComponent() {
   return (
-    <div>
-      <h1 className='text-secondary-p1-active'> Dineth Silva</h1>
-    </div >
+    <div className='w-full h-full h-screen bg-surface-secondary-main-active flex flex-row'>
+      <div>
+        <div className='flex flex-row gap-4 justify-start items-center w-56'>
+          <SidebarProvider>
+            <Sidebar className='bg-transparent border-none'>
+              <SidebarHeader>
+                <SidebarMenuItem>
+                  <SidebarMenuButton className='px-0 w-fit w-full !h-auto'>
+
+                    <Logo className='!w-7 !h-7' />
+
+                    <p className='h4 w-full'>App Name</p>
+
+                    <div className='px-2 py-1 bg-surface-primary-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)]'>
+                      <ArrowLeftToLine className='' />
+                    </div>
+
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarHeader>
+
+              <SidebarContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton >
+                      <a href="/home" className='sidebar-link'>
+                        <SquareLibrary />
+                        <p className='p-m-medium'>Store</p>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+
+                  <SidebarMenuItem>
+                    <SidebarMenuButton>
+                      <a href="/home" className='sidebar-link'>
+                        <SquareLibrary />
+                        <p className='p-m-medium'>Personal</p>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+
+
+                  <SidebarMenuItem>
+                    <SidebarMenuButton>
+                      <a href="/home" className='sidebar-link'>
+                        <SquareLibrary />
+                        <p className='p-m-medium'>Admin</p>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+
+                </SidebarMenu>
+              </SidebarContent>
+              <SidebarFooter />
+            </Sidebar>
+          </SidebarProvider>
+        </div>
+      </div >
+    </div>
   )
 }
