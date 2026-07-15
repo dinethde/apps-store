db3aef68c12ff770e65fc061de2022926dcff0b3
Dineth De Silva <dinethran33@gmail.com>
2026-07-15 15:28:10 +0530

    refactor: Remove unnecessary div wrapping sidebar

--- webapp/src/routes/__root.tsx
+++ webapp/src/routes/__root.tsx
@@ -62,63 +62,65 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 function RootComponent() {
   return (
     <div className='w-full h-full h-screen bg-surface-secondary-main-active flex flex-row'>
-      <div>
-        <div className='flex flex-row gap-4 justify-start items-center w-56'>
-          <SidebarProvider>
-            <Sidebar className='bg-transparent border-none'>
-              <SidebarHeader>
+      <div className='flex flex-row gap-4 justify-start items-center w-56'>
+        <SidebarProvider>
+          <Sidebar className='bg-transparent border-none'>
+            <SidebarHeader>
+              <SidebarMenuItem>
+                <SidebarMenuButton className='px-0 w-fit w-full !h-auto'>
+
+                  <Logo className='!w-7 !h-7' />
+
+                  <p className='h4 w-full'>App Name</p>
+
+                  <div className='px-2 py-1 bg-surface-primary-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)]'>
+                    <ArrowLeftToLine className='' />
+                  </div>
+
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarHeader>
+
+            <SidebarContent>
+              <SidebarMenu>
                 <SidebarMenuItem>
-                  <SidebarMenuButton className='px-0 w-fit w-full !h-auto'>
-
-                    <Logo className='!w-7 !h-7' />
-
-                    <p className='h4 w-full'>App Name</p>
-
-                    <div className='px-2 py-1 bg-surface-primary-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)]'>
-                      <ArrowLeftToLine className='' />
-                    </div>
-
+                  <SidebarMenuButton >
+                    <a href="/home" className='sidebar-link'>
+                      <SquareLibrary />
+                      <p className='p-m-medium'>Store</p>
+                    </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
-              </SidebarHeader>
 
-              <SidebarContent>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton >
-                      <a href="/home" className='sidebar-link'>
-                        <SquareLibrary />
-                        <p className='p-m-medium'>Store</p>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>
-                      <a href="/home" className='sidebar-link'>
-                        <SquareLibrary />
-                        <p className='p-m-medium'>Personal</p>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>
+                    <a href="/home" className='sidebar-link'>
+                      <SquareLibrary />
+                      <p className='p-m-medium'>Personal</p>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
 
 
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>
-                      <a href="/home" className='sidebar-link'>
-                        <SquareLibrary />
-                        <p className='p-m-medium'>Admin</p>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>
+                    <a href="/home" className='sidebar-link'>
+                      <SquareLibrary />
+                      <p className='p-m-medium'>Admin</p>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
 
-                </SidebarMenu>
-              </SidebarContent>
-              <SidebarFooter />
-            </Sidebar>
-          </SidebarProvider>
-        </div>
-      </div >
+              </SidebarMenu>
+            </SidebarContent>
+            <SidebarFooter />
+          </Sidebar>
+        </SidebarProvider>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
     </div>
   )
 }
