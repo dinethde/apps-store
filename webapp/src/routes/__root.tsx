4438629812bf57810e8f503378efdf07ecb67957
Dineth De Silva <dinethran33@gmail.com>
2026-07-22 17:16:37 +0530

    feat: Add header with welcome message and user profile

--- webapp/src/routes/__root.tsx
+++ webapp/src/routes/__root.tsx
@@ -2,7 +2,9 @@ import { HeadContent, Outlet, Scripts, createRootRoute } from '@tanstack/react-r
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import appCss from '../styles.css?url'
-import AppSidebar from '@/components/sidebar/Sidebar'
+import AppSidebar from '@/components/sidebar/AppSidebar'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ChevronsUpDown } from 'lucide-react';
 
 export const Route = createRootRoute({
   head: () => ({
@@ -56,13 +58,41 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 
 function RootComponent() {
   return (
-    <div className='w-full h-full h-screen bg-surface-secondary-main-active flex flex-row'>
+    <div className='w-full h-full h-screen bg-surface-secondary-main-active flex flex-row gap-1'>
       <div className='flex flex-row gap-4 justify-start items-center'>
         <AppSidebar />
       </div>
 
-      <div>
-        <Outlet />
+      <div className="py-2 w-full">
+        <div className='w-full'>
+          <div className='flex justify-between items-center'>
+            <div className='flex flex-col gap-1'>
+              <p className='text-primary-p4-active p-s'>
+                Apps Store
+              </p>
+
+              <p className='h3 text-primary-p1-active'>
+                Welcome back, Dineth
+              </p>
+            </div>
+
+            <div>
+              <div className="flex gap-2 items-center justify-content ">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+
+                <div className="w-full group-data-[collapsible=icon]:hidden">
+                  <p className="p-m-medium text-primary-p2-active">Dineth</p>
+                  <p className="p-s text-primary-p3-active">Software Engineer</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <Outlet />
+        </div>
       </div>
     </div >
   )
