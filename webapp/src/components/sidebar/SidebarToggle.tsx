7abd2919a848bbfe5b5c893f1f05c71bdbe6866e
Dineth De Silva <dinethran33@gmail.com>
2026-07-22 15:18:30 +0530

    feat: Add SidebarToggle component

---
+++ webapp/src/components/sidebar/SidebarToggle.tsx
@@ -0,0 +1,22 @@
+import { ArrowLeftToLine } from 'lucide-react';
+import { useSidebar } from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils"
+
+interface SidebarToggleProps {
+  className?: string,
+}
+
+export function SidebarToggle(props: SidebarToggleProps) {
+  const { className } = props
+  const { toggleSidebar } = useSidebar()
+
+  const handleClick = () => {
+    toggleSidebar()
+  }
+
+  return (
+    <div className={cn('px-2 py-1 h-fit w-fit bg-surface-primary-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)]', className)} onClick={handleClick}>
+      <ArrowLeftToLine className='w-4 h-4 group-data-[state=collapsed]:rotate-180' />
+    </div>
+  )
+}
