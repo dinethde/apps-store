3ae2d9bbf050d88148ed4b9c73480ec357e48595
Dineth De Silva <dinethran33@gmail.com>
2026-07-24 15:59:43 +0530

    refactor: Extract header markup into Header component

--- webapp/src/routes/__root.tsx
+++ webapp/src/routes/__root.tsx
@@ -8,6 +8,7 @@ import { Moon } from 'lucide-react';
 import { BellDot } from 'lucide-react';
 import { BellCheck } from 'lucide-react';
 import { ChevronsUpDown } from 'lucide-react';
+import Header from '@/components/header/Header'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -68,41 +69,11 @@ function RootComponent() {
 
       <div className="py-2 pr-2 w-full">
         <div className='w-full bg-surface-primary-main-active rounded-[20px] overflow-hidden h-full'>
-          <div className='flex justify-between items-center bg-surface-primary-light-active p-4'>
-            <div className='flex flex-col gap-1'>
-              <p className='text-primary-p4-active p-s'>
-                Apps Store
-              </p>
-
-              <p className='h3 text-primary-p1-active'>
-                Welcome back, Dineth
-              </p>
-            </div>
-
-            <div className='flex gap-4 justify-start items-center'>
-              <div className='flex gap-4'>
-                <Moon className='text-primary-p3-active' />
-
-                <BellCheck className='text-primary-p3-active' />
-              </div>
-
-
-              <div className='h-10 border-1 border-border-territory-light-active'></div>
-
-              <Avatar size='lg'>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-
-              <div className="w-full group-data-[collapsible=icon]:hidden">
-                <p className="p text-primary-p2-active">Dineth Silva</p>
-                <p className="p-s text-primary-p3-active">Software Engineer</p>
-              </div>
-            </div>
+          <Header />
+          <div className='p-4'>
+            <Outlet />
           </div>
-
         </div>
-        <Outlet />
       </div>
     </div >
   )
