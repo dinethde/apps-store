1cad20bc35a6370c5a00f65d8b46a4fde703d3cc
Dineth De Silva <dinethran33@gmail.com>
2026-07-26 13:32:21 +0530

    feat: Add new user profile display section

--- webapp/src/routes/index.tsx
+++ webapp/src/routes/index.tsx
@@ -1,12 +1,55 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Heart } from 'lucide-react';
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
+
   return (
     <>
-      <div className='text-primary-p1-active'>App</div>
-      <div className='text-primary-p1-active'>App</div>
+      <div>
+        <div>
+          <div className='flex justify-start gap-2'>
+
+            <div className='flex gap-2 items-center'>
+              <div>
+                <Avatar className='rounded-[10px] after:rounded-[inherit] avatar-fallback-border' size='lg'>
+                  <AvatarImage src="https://github.com/shadcn.png" className='rounded-[10px]' />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </div>
+
+              <div className='w-full flex flex-col gap-[2px]'>
+                <p className='p-r'>Code Sphere</p>
+                <p className='p-m'>Technology</p>
+              </div>
+            </div>
+
+            <Heart />
+          </div>
+
+          <div>
+            <div />
+          </div>
+        </div>
+
+        <div />
+
+        <div>
+          <div>Lorem ipsum dolor sit amet consectetur. Eu sit purus ac tempus</div>
+        </div>
+
+        <div>
+          <div>
+            <div>HR</div>
+          </div>
+
+          <div>
+            <div>Ops</div>
+          </div>
+        </div>
+      </div >
     </>
   )
 }
