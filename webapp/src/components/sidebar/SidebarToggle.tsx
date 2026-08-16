df6e89b7658da87e6f62d57c3bbb4d6e813026bf
Dineth De Silva <dinethran33@gmail.com>
2026-08-16 13:30:14 +0530

    style(sidebar): update toggle background color and add pointer cursor

--- webapp/src/components/sidebar/SidebarToggle.tsx
+++ webapp/src/components/sidebar/SidebarToggle.tsx
@@ -15,7 +15,7 @@ export function SidebarToggle(props: SidebarToggleProps) {
   }
 
   return (
-    <div className={cn('px-2 py-1 h-fit w-fit bg-surface-primary-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)]', className)} onClick={handleClick}>
+    <div className={cn('px-2 py-1 h-fit w-fit bg-surface-neutral-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)] cursor-pointer', className)} onClick={handleClick}>
       <ArrowLeftToLine className='w-4 h-4 group-data-[state=collapsed]:rotate-180' />
     </div>
   )
