0d7ce8445b76cabea8a4f61a5a2d509bea132b06
Dineth De Silva <dinethran33@gmail.com>
2026-08-16 13:48:36 +0530

    style(header): update color token classes and consolidate imports

--- webapp/src/components/header/Header.tsx
+++ webapp/src/components/header/Header.tsx
@@ -1,30 +1,29 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Moon } from 'lucide-react';
-import { BellCheck } from 'lucide-react';
+import { Moon, BellCheck } from 'lucide-react';
 
 export default function Header() {
   return (
     <div>
-      <div className='flex justify-between items-center bg-surface-primary-light-active p-4'>
+      <div className='flex justify-between items-center bg-surface-neutral-light-active p-4'>
         <div className='flex flex-col gap-1'>
-          <p className='text-primary-p4-active p-s'>
+          <p className='text-txt-primary-p4-active p-s'>
             Apps Store
           </p>
 
-          <p className='h3 text-primary-p1-active'>
+          <p className='h3 text-txt-primary-p1-active'>
             Welcome back, Dineth
           </p>
         </div>
 
         <div className='flex gap-4 justify-start items-center'>
           <div className='flex gap-4'>
-            <Moon className='text-primary-p3-active' />
+            <Moon className='text-txt-primary-p3-active' />
 
-            <BellCheck className='text-primary-p3-active' />
+            <BellCheck className='text-txt-primary-p3-active' />
           </div>
 
 
-          <div className='h-10 border-1 border-border-territory-light-active'></div>
+          <div className='h-10 border-1 border-border-secondary-light-active'></div>
 
           <Avatar size='lg'>
             <AvatarImage src="https://github.com/shadcn.png" />
@@ -32,8 +31,8 @@ export default function Header() {
           </Avatar>
 
           <div className="w-full group-data-[collapsible=icon]:hidden">
-            <p className="p text-primary-p2-active">Dineth Silva</p>
-            <p className="p-s text-primary-p3-active">Software Engineer</p>
+            <p className="p text-txt-primary-p2-active">Dineth Silva</p>
+            <p className="p-s text-txt-primary-p3-active">Software Engineer</p>
           </div>
         </div>
       </div>
