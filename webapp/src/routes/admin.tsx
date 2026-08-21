2710ce50be5c3028e8f22a4d418c8bd0668f6887
Dineth De Silva <dinethran33@gmail.com>
2026-08-21 17:45:18 +0530

    feat(admin): wire admin route to new tabbed forms
    Replace the Admin route placeholder with AdminTabs.

--- webapp/src/routes/admin.tsx
+++ webapp/src/routes/admin.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { AdminTabs } from '@/components/admin/AdminTabs'
 
 export const Route = createFileRoute('/admin')({ component: Admin })
 
 function Admin() {
   return (
-    <div className='w-fit flex flex-col gap-4'>
-      <h1>Admin</h1>
-    </div >
+    <div className="flex w-full flex-col gap-4">
+      <AdminTabs />
+    </div>
   )
 }
