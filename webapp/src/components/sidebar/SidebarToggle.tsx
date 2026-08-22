263c1ddc22893d62dd88c2543648ad1b7aa08308
Dineth De Silva <dinethran33@gmail.com>
2026-08-22 23:16:49 +0530

    feat: change the div into button

--- webapp/src/components/sidebar/SidebarToggle.tsx
+++ webapp/src/components/sidebar/SidebarToggle.tsx
@@ -15,8 +15,12 @@ export function SidebarToggle(props: SidebarToggleProps) {
   }
 
   return (
-    <div className={cn('px-2 py-1 h-fit w-fit bg-surface-neutral-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)] cursor-pointer', className)} onClick={handleClick}>
+    <button
+      type="button"
+      className={cn('px-2 py-1 h-fit w-fit bg-surface-neutral-light-active rounded-sm shadow-[0px_1px_4px_0px_rgba(0,0,0,0.08)] cursor-pointer', className)}
+      onClick={handleClick}
+    >
       <ArrowLeftToLine className='w-4 h-4 group-data-[state=collapsed]:rotate-180' />
-    </div>
+    </button>
   )
 }
