ad22ab8fc72c242711577ec97613a5dc3f17b043
Dineth De Silva <dinethran33@gmail.com>
2026-08-22 15:52:47 +0530

    feat: add avatar image and fallback to profile page

--- webapp/src/routes/profile.tsx
+++ webapp/src/routes/profile.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from '@/components/ui/avatar'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import { Typography } from '@/components/ui/typography'
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -9,14 +9,20 @@ export const Route = createFileRoute('/profile')({
 function Profile() {
   return (
     <div className='flex gap-5 items-end'>
-      <div>
-        <div>
-          <Avatar></Avatar>
-          <Typography variant="h5">Dineth De Silva</Typography>
-          <Typography variant="p-s" className='text-txt-primary-p3-active'>dinethdsilva</Typography>
+      <div className='flex flex-col gap-3 items-start'>
+        <Avatar className='size-20 rounded-xl after:rounded-[inherit]'>
+          <AvatarImage
+            src='https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-5.png'
+            alt='Dineth De Silva'
+            className='rounded-xl'
+          />
+          <AvatarFallback className='rounded-xl'>DD</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-col items-start'>
+          <Typography variant="h5" className='text-txt-neutral-p2-active'>Dineth De Silva</Typography>
+          <Typography variant="p-s-medium" className='text-txt-neutral-p3-active'>dinethdsilva@gmail.com</Typography>
         </div>
       </div>
-
 
       <div>
         <p>User Groups</p>
